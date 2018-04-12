90e542fce56420c787d986c2177bba98cacafa0c
xiaogehou <hou.xia@husky.neu.edu>
2018-04-12 10:32:59 -0400

    add rule, finished unit: paper

--- 6205Final-GA/src/paperGeneration_GA/Paper.java
+++ 6205Final-GA/src/paperGeneration_GA/Paper.java
@@ -5,8 +5,8 @@
  */
 package paperGeneration_GA;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -24,10 +24,10 @@ public class Paper {
 
     private double adaptationDegree;
 
-    private List<Problem> problemList;
+    private Set<Problem> problemList;
 
     public Paper() {
-        this.problemList = new ArrayList<>();
+        this.problemList = new HashSet<>();
     }
 
     public int getId() {
@@ -38,11 +38,11 @@ public class Paper {
         this.Id = Id;
     }
 
-    public List<Problem> getProblemList() {
+    public Set<Problem> getProblemList() {
         return problemList;
     }
 
-    public void setProblemList(List<Problem> problemList) {
+    public void setProblemList(Set<Problem> problemList) {
         this.problemList = problemList;
     }
 
@@ -56,7 +56,7 @@ public class Paper {
         }
         return totalScore;
     }
-    
+
     //eachDif*eachScore/totalScore
     public double getDifficulty() {
         if (difficulty == 0) {
@@ -64,9 +64,43 @@ public class Paper {
             for (Problem p : problemList) {
                 dif += p.getScore() * p.getDifficulty();
             }
-            difficulty = dif/getTotalScore();
+            difficulty = dif / getTotalScore();
         }
         return difficulty;
+    }
+
+    public int getProblemCount() {
+        return problemList.size();
+    }
+
+    //caculate KPCoverage: unitCoverage/expectCoverage
+    public void setKPCoverage(Rule rule) {
+        if (KPCoverage == 0) {
+            Set<Integer> result = new HashSet<>();
+            for (Problem p : problemList) {
+                result.addAll(p.getPoints());
+            }
+            Set<Integer> expectation = rule.getPoints();
+            //join:
+            result.retainAll(expectation);
+            KPCoverage = result.size() / expectation.size();
+        }
+    }
+
+    public double getKPCoverage() {
+        return KPCoverage;
+    }
+
+    //Set unit adaptation: f = 1-(1-M/N)*f1-|EP-P|*f2
+    //M/N is KPCoverage, EP is difficulty expectation, P is unit difficulty, f1 is KPCoverage weight, f2 is difficulty weight
+    public void setAdaptationDegree(Rule rule, double f1, double f2) {
+        if (adaptationDegree == 0) {
+            adaptationDegree = 1 - (1 - getKPCoverage()) * f1 - Math.abs(rule.getDifficulty() - getDifficulty()) * f2;
+        }
+    }
+
+    public double getAdaptationDegree() {
+        return adaptationDegree;
     }
     
 }
